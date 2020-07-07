b8bb843db2d4674fbca533f530b7974a918cb58e
sam <spalania@mtu.edu>
2020-07-06 21:29:23 -0400

    Delete unnecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sam/work/ROSPro/obstacle_detector
-# Build directory: /home/sam/work/ROSPro/obstacle_detector/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(src/my_robot)
--- build/catkin_tools_prebuild/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sam/work/ROSPro/obstacle_detector/build/catkin_tools_prebuild
-# Build directory: /home/sam/work/ROSPro/obstacle_detector/build/catkin_tools_prebuild
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
