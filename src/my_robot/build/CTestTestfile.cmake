432df6f3122684a4737d45a5e64736ffd7509502
sam <spalania@mtu.edu>
2020-11-21 23:00:33 +0530

    remove custom robot models, move to turtlebot3

--- src/my_robot/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sam/work/ROSPro/my_robot
-# Build directory: /home/sam/work/ROSPro/my_robot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
