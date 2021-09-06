4cce712aace2f87745fea7cee375591fdb55448f
sam <manoharsabari1@gmail.com>
2021-09-06 17:15:19 +0530

    add header filer for ekf

---
+++ src/sensor_fusion/include/sensor_fusion/ekf.h
@@ -0,0 +1,61 @@
+#ifdef SENSOR_FUSION_EKF_H 
+#define SENSOR_FUSION_EKF_H 
+
+#include "robot_localization/filter_base.h"
+
+#include <fstream>
+#include <vector>
+#include <set>
+#include <queue>
+
+
+namespace SensorFusion 
+{
+  /**
+   * @brief Externded kalman filter class
+   * 
+   * Implementation of an extended kalman filter (EKF). This class derives 
+   * from FilterBase and overrides the predict() and correct() methods in 
+   * keeping with the discrete time EKF algorithm.
+   */
+
+  // TODO: discrete time EKF algorithm
+  class Ekf : public FilterBase 
+  {
+    public:
+      /**
+       * @brief Construct a new Ekf object
+       * 
+       * @param args - Generic argument container (not used here, but 
+       * needed so that the ROS filters can pass arbitrary arguments 
+       * to templated filter types).
+       */
+      explicit Ekf(std::vector<double> args = std::vetor<double>());
+
+      /**
+       * @brief Destroy the Ekf object
+       * 
+       */
+      ~Ekf();
+
+      /**
+       * @brief Carries out the correct step in the predict/update cycle.
+       * 
+       * @param measurement - the measurement to fuse with our estimate.
+       */
+      void correct(const Measurement &measurement);
+
+      /**
+       * @brief Carries out the predict step in the predict/update cycle.
+       * 
+       * Projects the state and error matrices forward using a model of 
+       * the vehicle's motion.
+       * 
+       * @param referenceTime - The time at which the prediction is being
+       * made.
+       * @param delta - The time step over which to predict.
+       */
+      void predict(const double referenceTime, const double delta);
+
+  }  
+} // namespace SensorFusion 
