f3fa56553859b85384e42a1b37248edcc7e25b63
sam <manoharsabari1@gmail.com>
2021-09-12 22:15:09 +0530

    finish ros filter utilities

--- src/sensor_fusion/src/ros_filter_utilities.cpp
+++ src/sensor_fusion/src/ros_filter_utilities.cpp
@@ -37,3 +37,91 @@ std::ostream& operator<<(std::ostream& os, const std::vector<double> &vec) {
   return os;
 }
 
+namespace SensorFusion {
+
+  namespace RosFilterUtilities {
+
+      double getYaw(const tf2::Quaternion &quat) {
+        tf2::Matrix3x3 mat(quat);
+        double temp;
+        double yaw;
+        mat.getRPY(temp, temp, yaw);
+        return yaw;
+      }
+
+      bool lookupTransformSafe(const tf2_ros::Buffer &buffer, 
+                               const std::string &targetFrame,
+                               const std::string &sourceFrame,
+                               const ros::Time &time,
+                               const ros::Duration &timeout,
+                               tf2::Transform &targetFrameTrans,
+                               const bool silent) {
+        bool retVal = true;
+        // First try to transform the data at the requested time
+        try {
+          tf2::fromMsg(buffer.lookupTransform(targetFrame, sourceFrame, time, timeout).transform, targetFrameTrans);
+        } catch(tf2::TransformException &ex)
+        {
+          // The issue might be that the transforms that are available are not close
+          // enough temporally to be used. In that case, just use the latest available
+          // transform and warn the user.
+          try {
+            tf2::fromMsg(buffer.lookupTransform(targetFrame, sourceFrame, ros::Time(0)).transform, targetFrameTrans);
+            if (!silent) {
+              ROS_WARN_STREAM_THROTTLE(2.0, "Transform from " << sourceFrame << " to " << targetFrame << 
+                                                  " was unavailable for the time requested. Using the latest instead. \n");
+            }
+          } catch (tf2::TransformException &ex) {
+             if (!silent) {
+               ROS_WARN_STREAM_THROTTLE(2.0, "Could not obtain transform from " << sourceFrame << 
+                                                " to " << targetFrame << ". Error was " << ex.what() << "\n");
+            }
+            retVal = false;
+          }
+        }
+
+        // Transforming from a frame id to itself can fail when the tf tree isn't being
+        // braodcast (e.g., for some bag files?). This is the only failure that
+        // would throw an exception, so check for this situation before giving up.
+        if (!retVal) {
+          if (targetFrame == sourceFrame) {
+            targetFrameTrans.setIdentity();
+            retVal = true;
+          }
+        }
+        return retVal; 
+      }
+
+      bool lookupTransformSafe(const tf2_ros::Buffer &buffer, 
+                               const std::string &targetFrame,
+                               const std::string &sourceFrame,
+                               const ros::Time &time,
+                               tf2::Transform &targetFrameTrans,
+                               const bool silent)
+      {
+        return lookupTransformSafe(buffer, targetFrame, sourceFrame, time, ros::Duration(0), targetFrameTrans, silent);
+      }
+
+      void quatToRPY(const tf2::Quaternion &quat, double &roll, double &pitch, double &yaw) 
+      {
+        tf2::Matrix3x3 tempRot(quat);
+        tempRot.getRPY(roll, pitch, yaw);
+      }
+
+      void stateToTF(const Eigen::VectorXd &state, tf2::Transform &stateTF) {
+        stateTF.setOrigin(tf2::Vector3(state(StateMemberX), state(StateMemberY), state(StateMemberZ)));
+        tf2::Quaternion quat;
+        quat.setRPY(state(StateMemberRoll), state(StateMemberYaw), state(StateMemberPitch));
+        stateTF.setRotation(quat);
+      }
+
+      void TFtoState(const tf2::Transform &stateTF, Eigen::VectorXd &state) {
+        state(StateMemberX) = stateTF.getOrigin().getX();
+        state(StateMemberY) = stateTF.getOrigin().getY();
+        state(StateMemberZ) = stateTF.getOrigin().getZ();
+        quatToRPY(stateTF.getRotation(), state(StateMemberRoll), state(StateMemberPitch), state(StateMemberYaw));
+      }
+      
+  } // namespace RosFilterUtilities
+
+} // namespace SensorFusion 
