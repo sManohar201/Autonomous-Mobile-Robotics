fde828145bf9008c181b4f454bacd343b03d92fb
sam <manoharsabari1@gmail.com>
2021-09-17 17:47:33 +0530

    add approximate time synchronized to scan merger

--- src/perception/src/scan_merger.cpp
+++ src/perception/src/scan_merger.cpp
@@ -1,13 +1,19 @@
 #include "perception/scan_merger.h"
 
+#include <iostream>
+
 namespace perception 
 {
 
   ScanMerger::ScanMerger(ros::NodeHandle &nh, ros::NodeHandle &nh_local) :
-                        nh_(nh), nh_private_(nh_local)
+                        nh_(nh), nh_private_(nh_local),
+                        front_scan_(nh_, "/front_laser/scan", 1),
+                        rear_scan_(nh_, "/rear_laser/scan", 1),
+                        tf_listener_(tf_buffer_),
+                        sync_(laser_sync_policy(5), front_scan_, rear_scan_)
   {
     p_active_ = false;
-
+    
     front_scan_received_ = false;
     rear_scan_received_ = false;
 
@@ -17,6 +23,13 @@ namespace perception
     params_srv_ = nh_private_.advertiseService("params", &ScanMerger::updateParameters, this);
 
     initialize();
+  }
+
+  ScanMerger::~ScanMerger()
+  {
+    nh_private_.deleteParam("active");
+    nh_private_.deleteParam("publish_scan");
+    nh_private_.deleteParam("publish_pcl");
   }
 
   bool ScanMerger::updateParameters(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res)
@@ -32,15 +45,12 @@ namespace perception
     {
       if(p_active_)
       {
-        front_scan_sub_ = nh_.subscribe("front_scan", 10, &ScanMerger::frontScanCallback, this);
-        rear_scan_sub_ = nh_.subscribe("rear_scan", 10, &ScanMerger::rearScanCallback, this);
+        sync_.registerCallback(&ScanMerger::subscriberCallback, this);
         laser_scan_pub_ = nh_.advertise<sensor_msgs::LaserScan>("scan", 10);
         pcl_scan_pub_  = nh_.advertise<sensor_msgs::PointCloud2>("pcl", 10);
       }
       else 
       {
-        front_scan_sub_.shutdown();
-        rear_scan_sub_.shutdown();
         laser_scan_pub_.shutdown();
         pcl_scan_pub_.shutdown();
       }
@@ -48,5 +58,11 @@ namespace perception
     return true;
   }
 
+  void ScanMerger::subscriberCallback(const sensor_msgs::LaserScan::ConstPtr &front_scan, 
+                                      const sensor_msgs::LaserScan::ConstPtr &rear_scan)
+  {
+    ROS_INFO_STREAM("Front scan time : " << front_scan->header.stamp << "\n"
+                    "Rear scan time : " << rear_scan->header.stamp << "\n"); 
+  }
 
 }
